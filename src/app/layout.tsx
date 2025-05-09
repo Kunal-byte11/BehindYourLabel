e21986fa366345b7ce3057cd548c5f1dae96a495
Kunal Dubey <kunaldubey975@gmail.com>
2025-05-09 18:21:39 +0000

    this look veey basi make ui more clean and elegant add light greenish color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#57A7A2" />
+        <meta name="theme-color" content="#6DCC9B" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Ingredient Insights" />
